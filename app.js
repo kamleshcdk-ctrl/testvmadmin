5a622ad9d77515c7e385b89c8439d190826a0c30
kamlesh <kamlesh@appselec>
2026-02-13 11:57:13 +0530

    test

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-'use strict';
-
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from testvmadmin change \n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+export default {
+  async fetch(request) {
+    return new Response('Hello World from testvmadmin', {
+      status: 200,
+      headers: { 'Content-Type': 'text/plain' }
+    });
+  }
+};
